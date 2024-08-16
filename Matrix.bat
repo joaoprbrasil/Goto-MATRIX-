9395a160d64d54bccd759ea2b62124f842922bc1
João Pedro Brasil <83050781+joaoprbrasil@users.noreply.github.com>
2024-08-16 16:03:52 -0300

    Update Matrix.bat

--- Matrix.bat
+++ Matrix.bat
@@ -2,12 +2,11 @@
 color a
 prompt Neo@MATRIX
 title MATRIX
-start cmd.exe /k "color a && prompt Neo@MATRIX && title ENCRYPTING YOUR SENSITIVE DATA && dir /s && tree"
-start cmd.exe /k "color a && prompt Lain@NAVI && ipconfig && title IP HACKING"
 start cmd.exe /k "color a && prompt KILL THEM ALL && echo Loading . . . &&title DELETING ALL YOUR PROGRAMS && wmic product get name"
+start cmd.exe /k "color a && prompt Lain@NAVI && ipconfig && title IP HACKING"
+start cmd.exe /k "color a && prompt Neo@MATRIX && title ENCRYPTING YOUR SENSITIVE DATA && dir /s && tree"
 mode 100
 :a
 set /a x=%random%%%2
 echo %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x%
 goto a
-
