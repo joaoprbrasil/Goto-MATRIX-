8e85ba608108f9f6436f23e2f86f847b56400bc0
João Pedro Brasil <83050781+joaoprbrasil@users.noreply.github.com>
2024-08-16 15:59:17 -0300

    Add files via upload

---
+++ Matrix.bat
@@ -0,0 +1,13 @@
+@echo off
+color a
+prompt Neo@MATRIX
+title MATRIX
+start cmd.exe /k "color a && prompt Neo@MATRIX && title ENCRYPTING YOUR SENSITIVE DATA && dir /s && tree"
+start cmd.exe /k "color a && prompt Lain@NAVI && ipconfig && title IP HACKING"
+start cmd.exe /k "color a && prompt KILL THEM ALL && echo Loading . . . &&title DELETING ALL YOUR PROGRAMS && wmic product get name"
+mode 100
+:a
+set /a x=%random%%%2
+echo %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x% %x%
+goto a
+
